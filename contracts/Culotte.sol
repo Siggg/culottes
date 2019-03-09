7ea11050a3bca5125bc9b9855a07c9a2f2795f56
mohamed.talhaoui@gmail.com <mohamed.talhaoui@gmail.com>
2019-03-09 14:40:38 +0100

    Ballot, Vote, Events, Closing Lottery

--- contracts/Culotte.sol
+++ contracts/Culotte.sol
@@ -3,26 +3,69 @@ pragma solidity ^0.5.0;
 contract Culotte {
 
   string public criteria;
-  mapping (address => uint256) private votesFor;
-  mapping (address => uint256) private votesAgainst;
+
+  struct Ballot {
+    address candidate;
+    mapping (address => uint256) votesFor;
+    uint256 forAmount;
+    mapping (address => uint256)votesAgainst;
+    uint256 againstAmount;
+    uint blockNumber;
+    bool opened;
+  }
+
+  mapping (address => Ballot) private ballots;
+
+  event BallotOpened(address _candidate);
+  event BallotClosed(address _candidate);
+  event VoteReceived(address _from, address _candidate, bool _vote);
 
 
   constructor() public{
     criteria = 'Is a great OSS contributor';
   }
 
-  function vote(bool _vote, address candidate) public payable {
-    if (_vote)
-      votesFor[candidate] += msg.value;
-    else
-      votesAgainst[candidate] += msg.value;
+  function vote(bool _vote, address _candidate) public payable {
+    Ballot storage ballot = ballots[_candidate];
+    if (ballot.candidate == address(0x0)) {
+      // this is a new ballot, emit an event
+      emit BallotOpened(_candidate);
+    }
 
+    ballot.candidate = _candidate;
+    if (_vote) {
+      ballot.votesFor[msg.sender] += msg.value;
+      ballot.forAmount+= msg.value;
+    } else {
+      ballot.votesAgainst[msg.sender] += msg.value;
+      ballot.againstAmount += msg.value;
+    }
+    emit VoteReceived(msg.sender, _candidate, _vote);
+
+    if(block.number > ballot.blockNumber) {
+      // update ballot block number
+      ballot.blockNumber = block.number;
+      // start closing ballot lottery
+      if(closingLottery()) {
+        ballot.opened = false;
+      }
+    }
   }
 
-  function getAmount(bool _vote, address candidate) public view returns (uint256){
+  function getAmount(bool _vote, address _candidate) public view returns (uint256){
+    Ballot storage ballot = ballots[_candidate];
     if (_vote)
-      return votesFor[candidate];
+      return ballot.forAmount;
     else
-      return votesAgainst[candidate];
+      return ballot.againstAmount;
+  }
+
+  function closingLottery() public view returns (bool) {
+    uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp)));
+    uint res = randomHash % 10;
+    if(res > 6) {
+      return true;
+    }
+    return false;
   }
 }
--- test/Culotte.js
+++ test/Culotte.js
@@ -17,18 +17,31 @@ contract('Culotte', function(accounts) {
 
     let candidate = accounts[9];
     let receipt = await culotte.vote(true, candidate, {value: 100});
-    expect(receipt.logs.length).to.equal(0);
+    expect(receipt.logs.length).to.equal(2);
+    expect(receipt.logs[0].event).to.equal('BallotOpened');
+    expect(receipt.logs[0].args._candidate).to.equal(candidate);
+    expect(receipt.logs[1].event).to.equal('VoteReceived');
+    expect(receipt.logs[1].args._from).to.equal(accounts[0]);
+    expect(receipt.logs[1].args._candidate).to.equal(candidate);
+    expect(receipt.logs[1].args._vote).to.equal(true);
 
     let forAmount = await culotte.getAmount(true, candidate);
     expect(forAmount.toNumber()).to.equal(100);
 
     receipt = await culotte.vote(false, candidate, {value: 300});
-    expect(receipt.logs.length).to.equal(0);
+    expect(receipt.logs.length).to.equal(1);
+    expect(receipt.logs[0].args._from).to.equal(accounts[0]);
+    expect(receipt.logs[0].args._candidate).to.equal(candidate);
+    expect(receipt.logs[0].args._vote).to.equal(false);
 
     let againstAmount = await culotte.getAmount(false, candidate);
     expect(againstAmount.toNumber()).to.equal(300);
 
   });
 
+  it("should close vote", async function() {
+    const culotte = await Culotte.deployed();
+
+  });
 
 });

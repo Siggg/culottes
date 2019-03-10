630aa39199cba617eb15f38c7f87d11548b2908c
Siggg <sig@akasig.org>
2019-03-10 12:05:23 +0100

    Get ballot openings

--- src/app/projects/electionTabs/openelection/openelection.component.ts
+++ src/app/projects/electionTabs/openelection/openelection.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Web3Service} from '../../../util/web3.service';
+
+declare let require:any;
+const culotteABI = require('../../../../../build/contracts/Culotte.json');
 
 @Component({
   selector: 'app-openelection',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpenelectionComponent implements OnInit {
 
-  constructor() { }
+    candidates: any;
+    web3_eth_contract: any;
+
+  constructor(private web3Service: Web3Service) { }
 
   ngOnInit() {
+
+    this.web3Service.artifactsToContract(culotteABI)
+      .then((web3_eth_contract) => {
+           this.web3_eth_contract = web3_eth_contract;
+           return web3_eth_contract.getPastEvents("BallotOpened");
+        })
+      .then((ballotOpenings) => {
+        console.log("openings: ", ballotOpenings);
+        });
+
   }
 
 }

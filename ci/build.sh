1ceb9fa441b11b2f8f533e4aba2d54bee96eab2b
Siggg <sig@akasig.org>
2019-03-26 14:52:10 +0100

    Fix the build and test chain

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 set -x
 set -e
-ganache-cli -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" > /dev/null &
+ganache-cli -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" &
+# > /dev/null &
 GANACHE_PID=$!
 trap "kill $GANACHE_PID" EXIT INT TERM
 

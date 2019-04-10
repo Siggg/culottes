fc18cb0dd25ab81c65ddea1f912a8fe3069f584c
Siggg <sig@akasig.org>
2019-04-10 08:40:28 +0200

    Update install-deps.sh

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 set -x
 npm install -g @angular/cli
+npm install -g @angular/compiler-cli
+npm install -g @angular-devkit/build-angular
 npm install -g truffle
 npm install -g ganache-cli
-npm install -g @angular-devkit/build-angular
 npm install

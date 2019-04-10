9d7ab1a9beb9f516871b15bd71a250e896ee0104
Siggg <sig@akasig.org>
2019-04-10 13:53:58 +0200

    Fix npm dependencies issues.

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 set -x
-npm install -g @angular/cli
-npm install -g @angular/compiler-cli
-npm install -g @angular-devkit/build-angular
 npm install -g truffle
 npm install -g ganache-cli
 npm install

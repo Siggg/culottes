0d07cb1c10bd620436993b45d7ff1eea21fabd85
Siggg <sig@akasig.org>
2019-05-29 17:19:31 +0200

    Create open_trial.component.ts

---
+++ src/app/open_trial/open_trial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-open_trial',
+  templateUrl: './open_trial.component.html',
+  styleUrls: ['./open_trial.component.css']
+})
+export class OpentrialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

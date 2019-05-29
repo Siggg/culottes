9bcc4f41df009e0ccdd4fc8b57b97718706249ac
Siggg <sig@akasig.org>
2019-05-29 17:16:39 +0200

    Create closed_trial.component.ts

---
+++ src/app/closed_trial/closed_trial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-closed_trial',
+  templateUrl: './closed_trial.component.html',
+  styleUrls: ['./closed_trial.component.css']
+})
+export class ClosedtrialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

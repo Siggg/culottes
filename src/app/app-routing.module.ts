69ee86cf02b8307b8f7248bcef25df79a31a0708
Siggg <sig@akasig.org>
2019-05-29 16:57:54 +0200

    Rename CulotteList to Revolution

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { CulottelistComponent } from "./projects/culottelist/culottelist.component";
+import { RevolutionComponent } from "./revolution/revolution.component";
 import { AboutComponent } from "./about/about.component";
 import { IdComponent } from "./id/id.component";
 import { DonateComponent } from './donate/donate.component';
 import { NewCitizenComponent } from './new-citizen/new-citizen.component';
 
 const routes: Routes = [
-  { path: "", component: CulottelistComponent },
-  { path: "culottelist", component: CulottelistComponent },
+  { path: "", component: RevolutionComponent },
+  { path: "revolution", component: RevolutionComponent },
   { path: "about", component: AboutComponent },
   { path: "citizen", component: NewCitizenComponent },
   { path: "citizen/:address", component: IdComponent },

3f8e7887b577e29eb03be33a6947fb8644125555
Siggg <Siggg@users.noreply.github.com>
2019-08-04 14:40:18 +0200

    import RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
+import { RouterModule } frol"@angular/router";
 
 import { AppComponent } from "./app.component";
 import { CommonModule } from "@angular/common";

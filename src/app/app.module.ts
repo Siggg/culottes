81bab094e689088601a9a8af4ff55c42daba4f13
Siggg <Siggg@users.noreply.github.com>
2019-08-04 11:28:19 +0200

    import RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,7 @@ import { CitizenComponent } from "./citizen/citizen.component";
     MatToolbarModule,
     FormsModule,
     HttpClientModule,
+    RouterModule,
     AppRoutingModule,
     UtilModule
   ],

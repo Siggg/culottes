8c7ba190c2aabf3e8917c3149e521602a50f2cb2
Siggg <Siggg@users.noreply.github.com>
2019-08-07 19:16:10 +0200

    display price

--- src/app/revolution/revolution.component.ts
+++ src/app/revolution/revolution.component.ts
@@ -117,7 +117,11 @@ export class RevolutionComponent implements OnInit {
     this.web3Service.getPrice()
     .subscribe((price) => {
       if (typeof this.bastilleBalance == "number") { 
-        this.bastilleBalanceInFiat = (this.bastilleBalance * price[this.currency]).toString();
+        // this.bastilleBalanceInFiat = (this.bastilleBalance * price[this.currency]).toString();
+        this
+          .web3Service
+          .web3Status
+          .next(price.toString());
       }
     });
   }

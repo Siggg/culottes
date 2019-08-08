549fa949eeb3f8910798f521fa6e46a86b021b6f
Siggg <Siggg@users.noreply.github.com>
2019-08-08 15:21:49 +0200

    debug

--- src/app/revolution/revolution.component.ts
+++ src/app/revolution/revolution.component.ts
@@ -121,7 +121,7 @@ export class RevolutionComponent implements OnInit {
         this
           .web3Service
           .web3Status
-          .next(price.EUR.toString());
+          .next(price.body["EUR"].toString());
       }
     });
   }

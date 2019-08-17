8af72b3ae0a6c36190d1a2afad488dba22ec93a7
Siggg <Siggg@users.noreply.github.com>
2019-08-17 21:54:44 +0200

    vote should alter open trial

--- test/Revolution.js
+++ test/Revolution.js
@@ -255,13 +255,20 @@ contract('Revolution', function(accounts) {
     
     await revolution.vote(true, citizen, {from: A, value: 5});
     
+    expect(web3.utils.toBN(bastilleBalanceAfterDonation).sub(web3.utils.toBN(await revolution.bastilleBalance())).toNumber()).to.equal(0);
+    
+    status = await revolution.trialStatus(citizen);
+    expect(status.sansculotteScale.toNumber()).to.equal(5);
+    expect(status.privilegedScale.toNumber()).to.equal(0);
+    expect(status.opened).to.equal(true);
+    
     // closing trial should not add any cake to bastille after revolution locked
     
     await revolution.closeTrial(citizen);
     
     let bastilleBalanceAfterClosingTrial = await revolution.bastilleBalance();
     
-    expect(web3.utils.toBN(bastilleBalanceAfterClosingTrial).sub(web3.utils.toBN(bastilleBalanceAfterDonation)).toNumber()).to.equal(12);
+    expect(web3.utils.toBN(bastilleBalanceAfterClosingTrial).sub(web3.utils.toBN(bastilleBalanceAfterDonation)).toNumber()).to.equal(2);// or 3
     
     // distribution should happen even if bastille balance is less than distribution amount
     

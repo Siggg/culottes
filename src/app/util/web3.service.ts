7707b68b0b3d65e8503a5f255eae826b69a977d0
Siggg <sig@akasig.org>
2019-10-13 01:09:59 +0200

    Update contract

--- src/app/util/web3.service.ts
+++ src/app/util/web3.service.ts
@@ -21,10 +21,11 @@ export class Web3Service {
   private web3: any;
   private accounts: string[];
   public revolutions = {
-   "0x3a1C54c0414D1E162837Eda4C69c6d587A83b3d3": ["Rinkeby", "a frequent contributor to open source or copyleft-based projects who currently deserves a daily cup of thanks for their contributions"] // @rinkeby with 6200, 0.025 ETH, true, true
+    "0xadF8DdE7d8A73c039cf82433a9C193bBF5162d19": ["Rinkeby", "a frequent contributor to open source or copyleft-based projects who deserves a daily cup of thanks for their contributions"] // @rinkeby with 6200, 0.025 ETH, true, true
   };
-	// "0x3029ba9190cF587c399451aD09fBa2344fd72290" // @rinkeby with 4, 0.000625 ETH, true, true
-	// "0xB0573E469b5a1b811Ea43B6fc414686716c1FEe6" // @rinkeby with 3 142 true true but vote would not re-open trials
+	// "0x3a1C54c0414D1E162837Eda4C69c6d587A83b3d3" @rinkeby with 6200, 0.025 ETH, true, true but trials could close at any new block
+	// "0x3029ba9190cF587c399451aD09fBa2344fd72290" @rinkeby with 4, 0.000625 ETH, true, true
+	// "0xB0573E469b5a1b811Ea43B6fc414686716c1FEe6" @rinkeby with 3 142 true true but vote would not re-open trials
 	// "0xba074e774A614a167F88c161125eb515cDe824F0" @rinkeby with 3 1337 false false
 	// "0x9FB6C2d5556C31fCb6c35e88e99b0db3761ec053" @rinkeby with 3 7 false false but citizens was private
 	// "0xf26110452429f39eD677F111E65bf0c1825705A4" @rinkeby with 3 7 false false but bastilleBalance was called balance

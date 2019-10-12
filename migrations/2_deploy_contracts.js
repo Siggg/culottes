86ed48336e170264c455e598c499316d6104bb2a
Siggg <sig@akasig.org>
2019-10-12 18:38:04 +0200

    Specify distribution amount in ether

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -36,7 +36,7 @@ module.exports = function(deployer) {
     Revolution,
     'a frequent contributor to open source or copyleft-based projects who currently deserves a daily cup of thanks for their contributions', // criteria
     6200, // distributionBlockPeriod in blocks (about 13 to 15 seconds per block so one day is approximately 6200 blocks)
-    0.025 ether, // distributionAmount (in Wei)
+    0.025 * 1 ether, // distributionAmount (in Wei)
     false, // withLottery 
     false  // withDistribution
   );

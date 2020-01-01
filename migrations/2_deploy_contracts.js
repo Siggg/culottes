3a5d039c543c8c965e6a3a573e9a9ecd7dabc529
Siggg <sig@akasig.org>
2020-01-01 23:31:13 +0100

    New default settings for contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -32,7 +32,7 @@ module.exports = function(deployer) {
   deployer.deploy(
     Revolution,
     'a frequent contributor to open source or copyleft-based projects who deserves a daily cup of thanks for their contributions', // criteria
-    3, // 6200, // distributionBlockPeriod in blocks (about 13 to 15 seconds per block so one day is approximately 6200 blocks)
+    3, // 5760, // distributionBlockPeriod in blocks (about 15 seconds per block so one day is approximately 5760 blocks)
     7, // web3.utils.toBN(web3.utils.toWei('0.025', 'ether')), // distributionAmount, for instance web3.utils.toBN(web3.utils.toWei('0.025', 'ether'))
     false, // true, // withLottery, set to true unless you want to run automated tests 
     false // true  // withDistribution, set to true unless you want to run automated tests

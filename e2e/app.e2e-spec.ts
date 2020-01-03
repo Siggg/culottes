c95873e442d2a9ba5604f29179e43d03d2e4e2d6
Siggg <sig@akasig.org>
2020-01-03 16:29:32 +0100

    further tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -14,17 +14,17 @@ describe('angular-truffle-box App', () => {
 
   it('should display the criteria question', () => {
     page.navigateTo();
-    // page.getCriteria();
+    page.getCriteria();
     // TODO : check if we can test the value once it is read from the contract
     // expect(page.getCriteria()).toContain('frequent contributor to open source projects');
   });
   
-/*  it('should display the hashtag', () => {
+  it('should display the hashtag', () => {
     page.navigateTo();
     page.getHashtag();
     // TODO : check if we can test the value once it is read from the contract
     // expect(page.getHashtag()).toContain('#FrequentContributorRevolution');
-  }); */
+  }); 
 
 });
 

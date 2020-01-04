b527bdca215da27984eef7711f0a16557682bd53
Siggg <sig@akasig.org>
2020-01-04 18:46:11 +0100

    Better deployment parameters

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -31,11 +31,16 @@ module.exports = function(deployer) {
 
   deployer.deploy(
     Revolution,
-    'a frequent contributor to open source or copyleft-based projects who deserves a daily cup of thanks for their contributions', // criteria
-    '#FrequentContributorRevolution', // hashtag to be used to discuss this contract
-    3, // 5760, // distributionBlockPeriod in blocks (about 15 seconds per block so one day is approximately 5760 blocks)
-    7, // web3.utils.toBN(web3.utils.toWei('0.025', 'ether')), // distributionAmount, for instance web3.utils.toBN(web3.utils.toWei('0.025', 'ether'))
-    true // false, // testingMode, set to false unless you want to run automated tests 
+    'a frequent contributor to open source or copyleft-based projects who deserves a daily cup of thanks for their contributions', // criteria to match
+    '#CulottesRevolution', // hashag to be used to discuss this contract
+    3, // distributionBlockPeriod in blocks (about 15 seconds per block so one day is approximately 5760 blocks)
+    7, // distributionAmount for instance web3.utils.toBN(web3.utils.toWei('0.025', 'ether'))
+    true // testingMode, set to false unless you want to run automated tests
+/*    'a person who has been one of the top 3 most deserving contributors to this dapp over the last 7 days', // criteria to match
+    '#Top3Contributors', // hashtag to be used to discuss this contract
+    5760, // distributionBlockPeriod in blocks (about 15 seconds per block so one day is approximately 5760 blocks)
+    web3.utils.toBN(web3.utils.toWei('0.1', 'ether')), // distributionAmount, for instance web3.utils.toBN(web3.utils.toWei('0.025', 'ether'))
+    false, // testingMode, set to false unless you want to run automated tests */ 
   );
 };
 

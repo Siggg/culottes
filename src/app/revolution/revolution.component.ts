10c23301bbbd9757e5677aedaecafdab9be83cb1
Siggg <sig@akasig.org>
2020-01-07 00:25:36 +0100

    change to new revolution address

--- src/app/revolution/revolution.component.ts
+++ src/app/revolution/revolution.component.ts
@@ -266,8 +266,7 @@ export class RevolutionComponent implements OnInit {
   }
   
   public onRevolutionChange(event): void {  // event will give you full brief of action
-    console.log("event: ", event.target);
-    this.web3Service.revolutionAddress = event.target.key;
+    this.web3Service.revolutionAddress = event.target.value;
   }
   
   getAddress(): void { 

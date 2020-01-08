b37ad95aa010dc9c0593a43fea79c4813680ba25
Siggg <Siggg@users.noreply.github.com>
2020-01-08 09:05:30 +0100

    update revolutionAddress

--- src/app/revolution/revolution.component.ts
+++ src/app/revolution/revolution.component.ts
@@ -268,6 +268,7 @@ export class RevolutionComponent implements OnInit {
   public onRevolutionChange(event): void {  // event will give you full brief of action
     console.log("redirect to: /revolution/" + event.target.value);
     this.web3Service.revolutionAddress = event.target.value;
+    this.revolutionAddress = event.target.value;
     this.router.navigateByUrl('/revolution/' + event.target.value)
       .then( nav => {
         console.log("redirect succeeded: ", nav);

8da379e561fc8843e8caa5ff721ad1f9d22919db
Siggg <sig@akasig.org>
2020-02-11 10:48:52 +0100

    Revert "Ng update and better polyfill"
    This reverts commit a16acb4d91d1283cc4f1a760dd1fac148d94e592.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,7 +19,7 @@
  */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/symbol';
+/*import 'core-js/es/symbol';
 import 'core-js/es/object';
 import 'core-js/es/function';
 import 'core-js/es/parse-int';
@@ -33,7 +33,7 @@ import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/weak-map';
 import 'core-js/es/set';
-
+*/
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 

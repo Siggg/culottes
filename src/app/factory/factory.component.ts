78f46b49f7d627943fe02be0508d1553c7cdb318
Siggg <sig@akasig.org>
2020-04-05 18:42:21 +0200

    Fill revolution settings from existing revolution

--- src/app/factory/factory.component.ts
+++ src/app/factory/factory.component.ts
@@ -13,8 +13,8 @@ const contractABI = require('../../../build/contracts/Revolution.json');
 })
 export class FactoryComponent implements OnInit {
 
-        criteria: String; // = "default criteria from factory.component.ts";
-        distributionAmount: number; // = 0;
+        criteria: String;
+        distributionAmount: number;
         showErrorMessageForCriteria: boolean = false;
         showErrorMessageForHashtag: boolean = false;
         showErrorMessageForPeriod: boolean = false;
@@ -22,25 +22,22 @@ export class FactoryComponent implements OnInit {
         showErrorMessageForCreation: boolean = false;
         transactionPending = false;
         transactionConfirmed = false;
-        hashtag: String; // = "#MyNewRevolution";
-        distributionBlockPeriod: number; // = 0;
+        hashtag: String;
+        distributionBlockPeriod: number;
         web3_eth_contract: any;
 	account: any;
         confirmationProgress: number = 0;
         confirmationPercent: number = 0;
 	transactionHashes = [];
+	errorDuringCreation: String = "";
+	hashtagWithoutSymbol: String = "CulottesRevolution";
 
-
-	// address: String = "0x";
-	// name: String = "";
-	// revolutionAddress: String = "0x0000...";
-	// revolutionBlockchain: String = "";
+	address: String = "0x";
+	name: String = "";
+	revolutionAddress: String = "0x0000...";
+	revolutionBlockchain: String = "";
 	// culottes: any;
 	// accountBalance: number = undefined;
-	// amount: number = undefined;
-	// vote: boolean = undefined;
-        // errorDuringVote: String = "";
-	// hashtagWithoutSymbol: String = "CulottesRevolution";
 
   constructor(
     private web3Service: Web3Service,
@@ -51,8 +48,8 @@ export class FactoryComponent implements OnInit {
 
 
   async ngOnInit() {
-    /* this.getAddress();
-    this.watchAccount();
+    this.getParams();
+    // this.watchAccount();
     this.web3Service
       .artifactsToContract(contractABI)
       .then((web3_eth_contract) => {
@@ -89,13 +86,33 @@ export class FactoryComponent implements OnInit {
             .weiToEther(distributionAmount);
         }
 
-	// Get revolution's hashtag
+	// Get revolution's distributionBlockPeriod
+        return this.web3_eth_contract
+          .methods
+          .distributionBlockPeriod()
+          .call();
+      })
+      .then( (distributionBlockPeriod) => {
+        if (distributionBlockPeriod === null) {
+          this
+            .web3Service
+            .web3Status
+            .next("distributionBlockPeriod at this revolution is null!");
+          this
+            .web3Service
+            .statusError = true;
+        } else {
+          this.distributionBlockPeriod = distributionBlockPeriod;
+        }
+
+        // Get revolution's hashtag
         return this.web3_eth_contract
           .methods
           .hashtag()
           .call();
       })
       .then((hashtag) => {
+        this.hashtag = hashtag;
         console.log("hashtag: ", hashtag);
         if (hashtag != null && hashtag.length>0) {
           this.hashtagWithoutSymbol = hashtag.substring(1);
@@ -122,125 +139,10 @@ export class FactoryComponent implements OnInit {
       .revolutionAddress;
     this.revolutionBlockchain = this
       .web3Service
-      .revolutionBlockchain; */
-  }
+      .revolutionBlockchain;
+  } 
 
-  /* async sendVote(vote, weiAmount) {
-    let component = this;
-    component.vote = vote;
-    let method = this.web3_eth_contract
-      .methods
-      .vote(vote, this.address);
-    if (this.account == this.address) {
-      console.log('voting for oneself');
-      let myName = await this.web3_eth_contract
-        .methods
-        .getName()
-        .call();
-      if (myName != this.name) {
-        // Change one's name
-	console.log('your name was: ', myName);
-	console.log('vote and set it to: ', this.name);
-        method = this.web3_eth_contract
-          .methods
-          .voteAndSetName(vote, this.address, this.name);
-      }
-    }
-    method.send({from: this.account, value: weiAmount, gas: 1000000})
-      .on('transactionHash', function(hash) {
-        component.transactionPending = true;
-        component.confirmationProgress = 0;
-	component.confirmationPercent = 0;
-        component.transactionHashes.push(hash);
-        console.log('transactionHash received');
-      })
-      .on('confirmation', function(confirmationNumber, receipt) {
-        component.transactionPending = true;
-        component.confirmationProgress += 1; //confirmationNumber; // up to 24
-	component.confirmationPercent = Math.round(100 * component.confirmationProgress / 24);
-        console.log('confirmation received, with number and %: ', confirmationNumber, component.confirmationPercent);
-      })
-      .on('receipt', function(receipt){
-        // receipt example
-        console.log('receipt received: ', receipt);
-	component.transactionPending = false;
-	component.transactionConfirmed = true;
-      })
-      .on('error', function(error, receipt){
-        console.error;
-        this.showErrorMessageForVote = true;
-        this.errorDuringVote = error;
-      }); // If there's an out of gas error the second parameter is the receipt.
-  } */
-
-  /* async cakeVote(vote) {
-    let canVote = true;
-    let addressIsValid = true;
-    let component = this;
-
-    // Check address
-    try {
-      this.address = window
-        .web3
-        .utils
-        .toChecksumAddress(this.address);
-      this.showErrorMessageForAddress = false;
-    } catch(e) {
-      canVote = false;
-      addressIsValid = false;
-      this.showErrorMessageForAddress = true;
-      this.name = e.message;
-      console.error('invalid ethereum address', e.message);
-    }
-
-    // Check account
-    if (this.account == undefined) {
-      // Maybe metamask has not been enabled yet
-      try {
-        // Request account access if needed
-        await window.ethereum.enable().then(() =>  {
-          window.web3.eth.getAccounts((err, accs) => {
-            this.account = accs[0];
-            console.log("Accounts refreshed, vote by: " + this.account);
-          });
-        });
-      } catch (error) {
-        canVote = false;
-        console.log('Metamask not enabled');
-      }
-    }        
-    if (this.account == undefined) {
-      canVote = false;
-    }
-
-    // Check amount
-    if (this.amount < this.distributionAmount / 10) {
-      canVote = false;
-      this.showErrorMessageForAmount = true;
-    } else {
-      this.showErrorMessageForAmount = false;
-    }
-
-    // Check balance
-    if (this.amount >= component.accountBalance) {
-      canVote = false;
-      this.showErrorMessageForBalance = true;
-    } else {
-      this.showErrorMessageForBalance = false;
-    }
-    const weiAmount = this
-      .web3Service
-      .etherToWei(this.amount.toString());
-    console.log("Stake (in wei): " + weiAmount.toString());
-
-    // Vote if everything is fine
-    if (canVote == true) {
-      console.log("Vote by: " + this.account);
-      this.sendVote(vote, weiAmount);
-    }
-  } */
-
-  onRevolutionCreate() {
+  async onRevolutionCreate() {
     let canCreate = true;
     if (this.criteria == undefined || this.criteria.length == 0) {
       this.showErrorMessageForCriteria = true;
@@ -269,6 +171,27 @@ export class FactoryComponent implements OnInit {
     } else {
       this.showErrorMessageForAmount = false;
     }
+
+    // Check account
+    if (this.account == undefined) {
+      // Maybe metamask has not been enabled yet
+      try {
+        // Request account access if needed
+        await window.ethereum.enable().then(() =>  {
+          window.web3.eth.getAccounts((err, accs) => {
+            this.account = accs[0];
+            console.log("Accounts refreshed, vote by: " + this.account);
+          });
+        });
+      } catch (error) {
+        canCreate = false;
+        console.log('Metamask not enabled?');
+      }
+    }        
+    if (this.account == undefined) {
+      canCreate = true;
+    }
+
     if (canCreate == true) {
       console.log('about to create revolution');
       let method = this.web3_eth_contract
@@ -303,9 +226,12 @@ export class FactoryComponent implements OnInit {
     }
   }
 
-  /* getAddress(): void {
-    this.address = this.route.snapshot.paramMap.get('address');
-  } */
+  getParams(): void {
+    this.criteria = this.route.snapshot.paramMap.get('criteria');
+    this.hashtag = this.route.snapshot.paramMap.get('hashtag');
+    this.distributionBlockPeriod = parseInt(this.route.snapshot.paramMap.get('distributionBlockPeriod'));
+    this.distributionAmount = parseFloat(this.route.snapshot.paramMap.get('distributionAmount'));
+  }
 
   /* async watchAccount() {
     let component = this;
